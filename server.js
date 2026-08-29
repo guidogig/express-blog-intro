de5eb378b324b375c17d00b97f666f8bb77ccb7b
Guido Giglioli <guido.tobugroup@gmail.com>
2026-08-29 22:45:19 +0200

    fatto prove con vari metodi res. di express

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Server del mio blog!");
+  // con .type() specifichiamo il tipo di dato che inviamo (non necessario specificare html se .send(), visto che é il tipo di default)
+  res.type("html").send("<h1>Server del mio blog!</h1>");
+
+  //res.download("./public/images/ciambellone.jpeg");
+
+  //res.redirect("/altra-pagina");
+
+  //.json() é una scorciatoia per .type("json")
+  //res.json({ messaggino: "Server del mio blog!", tipo: "JSON!" });
 });
 
 app.listen(port, () => {
